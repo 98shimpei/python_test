95fe92a0b22fbbb877e362281696145ad41a9b89
hoge <piyo@fuga>
2019-08-12 21:37:48 +0900

    queueも作った

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import my_stack
+import my_queue
 
 stack = my_stack.Stack()
-stack.push(2)
-stack.push(5)
-print(stack.pop())
-print(stack.peek())
-print(stack.pop())
-print(stack.is_empty())
+queue = my_queue.Queue()
+for c in "Hello":
+    stack.push(c)
+    queue.enqueue(c)
+reverse = ""
+not_reverse = ""
+
+while not stack.is_empty():
+    reverse += stack.pop()
+
+while not queue.is_empty():
+    not_reverse += queue.dequeue()
+
+print(reverse)
+print(not_reverse)
